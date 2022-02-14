1b763979500a9ef77df1fd700ca74d4f95d3b03f
mslmyrtd <mslmyrtd@gmail.com>
2022-02-14 14:07:51 +0300

    migrate yapıldı superuser olusturukdu

--- todo/models.py
+++ todo/models.py
@@ -14,3 +14,7 @@ class Todo(models.Model):
     isDone=models.BooleanField(default=False)
     updated_date=models.DateTimeField(auto_now=True)
     created_date=models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.title
